6810544cfd4f1242382a0f1e94a59ce98a4614ff
Adeyeye Faishal <adeyeyea20@gmail.com>
2023-09-07 01:24:45 +0100

    Update reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -12,6 +12,7 @@ export const searchRobots = (state=initialStateSearch, action={}) =>{
     switch(action.type){
         case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, {searchField:action.payload});
+            // retutn
             // return { ...state, searchField:action.payload }
         default:
             return state;    
